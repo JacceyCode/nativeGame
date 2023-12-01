9a11e85115bb184d075c733c534423f4d8f61db0
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-12-01 19:14:41 +0100

    implemented SafeAreaView

---
+++ screens/GameScreen.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function GameScreen() {
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* Guess */}
+      <View>
+        <Text>Higer or lower?</Text>
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
+}
+
+export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
